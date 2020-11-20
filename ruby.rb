355701b5ec225f3d73dc15241bac7e94d51cffc5
Angel Barros <albarros1973@gmail.com>
2020-11-20 12:28:04 +0100

    Revert "made changes to the inject method"
    This reverts commit b8c43625ba0222127f22ebe4c7b93bc8a08afaa1.

--- ruby.rb
+++ ruby.rb
@@ -1,7 +1,9 @@
- rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/ModuleLength
+# rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/ModuleLength
 # My module Enumerable
 module Enumerable
-
+  # Create #my_each, a method that is identical to #each but (obviously) does not use #each.
+  # You'll need to remember the yield statement.
+  # Make sure it returns the same thing as #each as well.
   def my_each
     return to_enum unless block_given?
 
@@ -11,9 +13,11 @@ module Enumerable
       yield(arr[i])
       i += 1
     end
+
+    arr != to_a ? arr : self
   end
 
-  # #my_each_with_index 
+  # Create #my_each_with_index in the same way.
   def my_each_with_index
     return to_enum unless block_given?
 
@@ -24,7 +28,7 @@ module Enumerable
     end
   end
 
-  # #my_select 
+  # Create #my_select in the same way, though you may use #my_each in your definition (but not #each).
   def my_select(parameter = nil)
     arr = []
     if block_given?
@@ -37,7 +41,7 @@ module Enumerable
     arr
   end
 
-  #  #my_all? 
+  # Create #my_all? (continue as above)
   def my_all?(parameter = nil)
     result = true
     if block_given?
@@ -57,7 +61,7 @@ module Enumerable
     result
   end
 
-  # #my_any?
+  # Create #my_any?
   def my_any?(parameter = nil)
     result = false
     if block_given?
@@ -77,7 +81,7 @@ module Enumerable
     result
   end
 
-  # #my_none?
+  # Create #my_none?
   def my_none?(parameter = nil)
     result = true
     if block_given?
@@ -97,7 +101,7 @@ module Enumerable
     result
   end
 
-   #my_count
+  # Create #my_count
   def my_count(parameter = nil)
     count = 0
     if block_given?
@@ -115,27 +119,35 @@ module Enumerable
     count
   end
 
-  #the #my_inject
+  # Create #my_inject
   def my_inject(*parameter)
+    raise LocalJumpError if !block_given? && parameter[0].nil? && parameter[1].nil?
+
     total = 0
     if block_given?
       parameter = parameter[0]
       my_each_with_index do |value, index|
-        total = if !index.zero?
-                  yield(total, value)
-                else
-                  parameter.nil? ? value : yield(parameter, value)
-                end
+        param = parameter.nil? ? value : yield(parameter, value)
+        total = !index.zero? ? yield(total, value) : param
       end
     elsif parameter[1].nil?
-      my_each { |value| total = value.send parameter[0], total }
+      if parameter[0].is_a? Symbol
+        my_each_with_index { |value, index| total = value.send parameter[0], (index.zero? ? value : total) }
+      else
+        my_each { |value| total = value.send parameter[0], total }
+      end
     else
-      my_each_with_index { |value, index| total = index.zero? ? value : (value.send parameter[0], total) }
+      my_each_with_index { |value, index| total = value.send parameter[1], (index.zero? ? parameter[0] : total) }
     end
     total
   end
 
- 
+  # Modify your #my_map method to take either a proc or a block.
+  # It won't be necessary to apply both a proc and a block in the same #my_map call
+  # since you could get the same effect by chaining together one #my_map call with the block
+  # and one with the proc. This approach is also clearer, since the user doesn't have to remember
+  # whether the proc or block will be run first.
+  # So if both a proc and a block are given, only execute the proc.
   def my_map(proc = nil)
     return to_enum unless block_given?
 
@@ -149,7 +161,10 @@ module Enumerable
   end
 end
 
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/ModuleLength
 
+# Test your #my_inject by creating a method called #multiply_els which multiplies all
+# the elements of the array together by using #my_inject, e.g. multiply_els([2,4,5]) #=> 40
 def multiply_els(arr)
   arr.my_inject(1) { |total, x| total * x }
 end
